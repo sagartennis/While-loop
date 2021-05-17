ccb0c225453e7c9dc6470d23c24c692e86ca1262
sagartennis <sagarthetennis10k@gmail.com>
2021-05-17 18:08:48 +0530

    Add files via upload

---
+++ whilel.java
@@ -0,0 +1,10 @@
+public class whilel {
+    public static void main(String[] args) {
+        
+        int a = 1;
+        while(a<11){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
